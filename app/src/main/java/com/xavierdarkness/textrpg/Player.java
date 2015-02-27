0ab3638c81511b7f1505db175d9e1a9418f1e32c
Xavierdarkness <mrb1105@gmail.com>
2015-02-27 02:39:12 -0500

    added full android project

--- app/src/main/java/com/xavierdarkness/textrpg/Player.java
+++ app/src/main/java/com/xavierdarkness/textrpg/Player.java
@@ -4,13 +4,11 @@ package com.xavierdarkness.textrpg;
  * Created by Xavierdarkness on 2/27/15.
  */
 public class Player extends Character{
-    //private constructor
+    //protected constructor
     protected Player(int health, int strength, int defense){
         maxHealth = health;
         this.health = health;
         this.strength = strength;
         this.defense = defense;
     }
-    }//end getInstance(int id)
-
 }//end class
