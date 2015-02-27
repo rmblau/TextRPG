16d62d1310247ed4bedc05e198b61fb53441a6b1
Xavierdarkness <mrb1105@gmail.com>
2015-02-27 02:39:12 -0500

    Refractor all the things

--- app/src/main/java/com/xavierdarkness/textrpg/Game.java
+++ app/src/main/java/com/xavierdarkness/textrpg/Game.java
@@ -4,38 +4,85 @@ package com.xavierdarkness.textrpg;
  * Created by Xavierdarkness on 2/27/15.
  */
 public class Game {
-    //determine who goes first
-    private static int rand = (int)(Math.random() * 2);
-    //determine enemy
-    private static int num = (int)(Math.random() * 3);
-    public static void main(String[] args){
-        Enemy e = Enemy.getInstance(num);
-        Player p = new Player(100, 60, 30);
-        Character first;
-        Character second;
 
-        if(rand == 0){
-            first = p;
-            second = e;
+    //Fields
+    private Enemy e;
+
+    //Constructor
+
+    Player p = new Player(100, 50, 20);
+
+    /////////////////////////////////////////
+    //HELPER METHODS////////////////////////
+    ///////////////////////////////////////
+    private Enemy getRandomEnemy(){
+        int n = (int)(Math.random() * 2);
+        return Enemy.getInstance(n);
+    }
+
+    private boolean didPlayerWin(){
+        if(e.isDead()){
+            return true;
+        }
+        return false;
+    }
+
+    private void printBattleLog(){
+        System.out.println("Enemy Health: " +e.health);
+        System.out.println("Player Health: " + p.health);
+
+        if(didPlayerWin()){
+            System.out.println("Player is victorious! Onto the next level!");
         }
         else {
-            first = e;
-            second = p;
-        };
-        while((!e.isDead()) && (!p.isDead())){
+            System.out.println("The hero has fallen! GAME OVER!!!/n/n");
+        }
+    }
+
+    private void assignOrder(Character[] c){
+        if(c.length != 2){
+            return;
+        }
+
+        int n = (int)(Math.random()*2);
+        if(n == 0){
+            c[0] = p;
+            c[1] = e;
+        }else{
+            c[0] = e;
+            c[1] = p;
+        }
+
+    }
+
+    private void fight(Character first, Character second){
+
+        while((!first.isDead()) && (!second.isDead())){
             first.attack(second);
-            if((!e.isDead()) && (!p.isDead())){
+            if((!first.isDead()) && (!second.isDead())){
                 second.attack(first);
             }
-            if(e.isDead()){
-                System.out.println("Player wins!");
-            }
-            if(p.isDead()){
-                System.out.println("Enemy wins!");
-            }
-            System.out.println("Enemy: " + num);
-            System.out.println("Enemy Health: " +e.health);
-            System.out.println("Player Health: " + p.health);
         }
+    }
+
+    public void battle(){
+
+        while(!p.isDead()){
+
+            //generates new enemy
+            e = getRandomEnemy();
+            Character[] c = new Character[2];
+            assignOrder(c);
+
+            fight(c[0], c[1]);
+            printBattleLog();
+
+        }
+
+    }
+
+    public static void main(String[] args){
+        Game g = new Game();
+        g.battle();
     }
 }
